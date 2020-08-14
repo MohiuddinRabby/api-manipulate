2daaae53e30fa2bdaf47d2da710114815d1de8a9
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-14 21:42:48 +0600

    app js updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import Shop from "./components/Shop/Shop";
 import Nav from "./components/Nav/Nav";
 import { CartProvider } from "./global/CartContext";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
 function App() {
   return (
     <>
-      <CartProvider>
-        <Nav></Nav>
-        <Shop></Shop>
-      </CartProvider>
+      <Router>
+        <Switch>
+          <CartProvider>
+            <Nav></Nav>
+            <Route exact path="/">
+              <Shop></Shop>
+            </Route>
+            <Route path="/details/:key">
+              <ProductDetails></ProductDetails>
+            </Route>
+          </CartProvider>
+        </Switch>
+      </Router>
     </>
   );
 }
